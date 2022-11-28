a31a1328f65abb81825b8af08308f09979b7ebd6
Shuichi <zhuravkov.mai@gmail.com>
2022-11-28 22:50:14 +0300

    Added pack extracting for tests

--- tests/check_consistency/test_check_consistency.py
+++ tests/check_consistency/test_check_consistency.py
@@ -67,21 +67,29 @@ def test_amount_person_film_work(db_connect):
     assert sqlite_cursor.fetchone()[0] == psycopg2_cursor.fetchone()[0]
 
 
+def extract_pack(cursor, table_name):
+    """Функция для извлечения данных из таблицы"""
+    cursor.execute(f'SELECT * FROM {table_name}')
+    while True:
+        pack = cursor.fetchmany(100)
+        if pack:
+            yield pack
+        else:
+            break
+
 def test_content_genre(db_connect):
     """Тест для проверки соответствия жанров"""
     sqlite_cursor, psycopg2_cursor = db_connect[0].cursor(), db_connect[1].cursor()
     sqlite_cursor.execute('SELECT * FROM genre')
     psycopg2_cursor.execute('SELECT * FROM content.genre')
 
-    sqlite_data = sqlite_cursor.fetchall()
-    pg_data = psycopg2_cursor.fetchall()
-
-    for sqlite_row, pg_row in zip(sqlite_data, pg_data):
-        assert sqlite_row[0] == pg_row[0]
-        assert sqlite_row[1] == pg_row[1]
-        assert sqlite_row[2] == pg_row[2]
-        assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
-        assert datetime.strptime(sqlite_row[4]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[4]
+    for sqlite_pack, pg_pack in zip(extract_pack(sqlite_cursor, 'genre'), extract_pack(psycopg2_cursor, 'content.genre')):
+        for sqlite_row, pg_row in zip(sqlite_pack, pg_pack):
+            assert sqlite_row[0] == pg_row[0]
+            assert sqlite_row[1] == pg_row[1]
+            assert sqlite_row[2] == pg_row[2]
+            assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
+            assert datetime.strptime(sqlite_row[4]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[4]
 
 
 def test_content_person(db_connect):
@@ -90,14 +98,12 @@ def test_content_person(db_connect):
     sqlite_cursor.execute('SELECT * FROM person')
     psycopg2_cursor.execute('SELECT * FROM content.person')
 
-    sqlite_data = sqlite_cursor.fetchall()
-    pg_data = psycopg2_cursor.fetchall()
-
-    for sqlite_row, pg_row in zip(sqlite_data, pg_data):
-        assert sqlite_row[0] == pg_row[0]
-        assert sqlite_row[1] == pg_row[1]
-        assert datetime.strptime(sqlite_row[2]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[2]
-        assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
+    for sqlite_pack, pg_pack in zip(extract_pack(sqlite_cursor, 'person'), extract_pack(psycopg2_cursor, 'content.person')):
+        for sqlite_row, pg_row in zip(sqlite_pack, pg_pack):
+            assert sqlite_row[0] == pg_row[0]
+            assert sqlite_row[1] == pg_row[1]
+            assert datetime.strptime(sqlite_row[2]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[2]
+            assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
 
 
 def test_content_film_work(db_connect):
@@ -106,22 +112,21 @@ def test_content_film_work(db_connect):
     sqlite_cursor.execute('SELECT * FROM film_work')
     psycopg2_cursor.execute('SELECT * FROM content.film_work')
 
-    sqlite_data = sqlite_cursor.fetchall()
-    pg_data = psycopg2_cursor.fetchall()
-
-    for sqlite_row, pg_row in zip(sqlite_data, pg_data):
-        assert sqlite_row[0] == pg_row[0]
-        assert sqlite_row[1] == pg_row[1]
-        assert sqlite_row[2] == pg_row[2]
-        if sqlite_row[3] is None:
-            assert pg_row[3] is None
-        else:
-            assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
-        assert sqlite_row[4] == pg_row[8]
-        assert sqlite_row[5] == pg_row[4]
-        assert sqlite_row[6] == pg_row[5]
-        assert datetime.strptime(sqlite_row[7]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[6]
-        assert datetime.strptime(sqlite_row[8]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[7]
+    for sqlite_pack, pg_pack in zip(extract_pack(sqlite_cursor, 'film_work'), extract_pack(psycopg2_cursor, 'content.film_work')):
+        for sqlite_row, pg_row in zip(sqlite_pack, pg_pack):
+            sqlite_row, pg_row = list(sqlite_row), list(pg_row)
+            assert sqlite_row[0] == pg_row[0]
+            assert sqlite_row[1] == pg_row[1]
+            assert sqlite_row[2] == pg_row[2]
+            if sqlite_row[3] is None:
+                assert pg_row[3] is None
+            else:
+                assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
+            assert sqlite_row[4] == pg_row[4]
+            assert sqlite_row[5] == pg_row[5]
+            assert sqlite_row[6] == pg_row[6]
+            assert datetime.strptime(sqlite_row[7]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[7]
+            assert datetime.strptime(sqlite_row[8]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[8]
 
 
 def test_content_genre_film_work(db_connect):
@@ -130,14 +135,12 @@ def test_content_genre_film_work(db_connect):
     sqlite_cursor.execute('SELECT * FROM genre_film_work')
     psycopg2_cursor.execute('SELECT * FROM content.genre_film_work')
 
-    sqlite_data = sqlite_cursor.fetchall()
-    pg_data = psycopg2_cursor.fetchall()
-
-    for sqlite_row, pg_row in zip(sqlite_data, pg_data):
-        assert sqlite_row[0] == pg_row[0]
-        assert sqlite_row[2] == pg_row[1]
-        assert sqlite_row[1] == pg_row[2]
-        assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
+    for sqlite_pack, pg_pack in zip(extract_pack(sqlite_cursor, 'genre_film_work'), extract_pack(psycopg2_cursor, 'content.genre_film_work')):
+        for sqlite_row, pg_row in zip(sqlite_pack, pg_pack):
+            assert sqlite_row[0] == pg_row[0]
+            assert sqlite_row[1] == pg_row[1]
+            assert sqlite_row[2] == pg_row[2]
+            assert datetime.strptime(sqlite_row[3]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[3]
 
 
 def test_content_person_film_work(db_connect):
@@ -146,12 +149,11 @@ def test_content_person_film_work(db_connect):
     sqlite_cursor.execute('SELECT * FROM person_film_work')
     psycopg2_cursor.execute('SELECT * FROM content.person_film_work')
 
-    sqlite_data = sqlite_cursor.fetchall()
-    pg_data = psycopg2_cursor.fetchall()
+    for sqlite_pack, pg_pack in zip(extract_pack(sqlite_cursor, 'person_film_work'), extract_pack(psycopg2_cursor, 'content.person_film_work')):
 
-    for sqlite_row, pg_row in zip(sqlite_data, pg_data):
-        assert sqlite_row[0] == pg_row[0]
-        assert sqlite_row[2] == pg_row[1]
-        assert sqlite_row[1] == pg_row[2]
-        assert sqlite_row[3] == pg_row[3]
-        assert datetime.strptime(sqlite_row[4]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[4]
+        for sqlite_row, pg_row in zip(sqlite_pack, pg_pack):
+            assert sqlite_row[0] == pg_row[0]
+            assert sqlite_row[1] == pg_row[1]
+            assert sqlite_row[2] == pg_row[2]
+            assert sqlite_row[3] == pg_row[3]
+            assert datetime.strptime(sqlite_row[4]+"00", "%Y-%m-%d %H:%M:%S.%f%z") == pg_row[4]
